5c59fd189345deaae0e27fb39338e8caeddd0292
whatthefoobar <irina.mntn@gmail.com>
2022-04-30 11:16:51 +0200

    feed search section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import Feed from './components/Feed/Feed';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Widgets from './components/Widgets/Widgets';
 
 function App() {
   return (
@@ -10,8 +12,9 @@ function App() {
       {/*v App Body */}
       <div className="app__body">
         <Sidebar />
+        <Feed />
+        <Widgets />
       </div>
-      {/*1 Sidebar */}
       {/*2 Feed */}
       {/*3 Widget -Right side */}
     </div>
---
+++ src/components/Widgets/Widgets.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Widgets.css';
+
+const Widgets = () => {
+  return <div className="widgets">Widgets</div>;
+};
+
+export default Widgets;

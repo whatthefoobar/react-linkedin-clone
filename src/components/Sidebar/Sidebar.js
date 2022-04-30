abea76018831a58def1531d488ee665966e102eb
whatthefoobar <irina.mntn@gmail.com>
2022-04-30 10:29:27 +0200

    sidebar done for now

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,17 +3,42 @@ import React from 'react';
 import './Sidebar.css';
 
 const Sidebar = () => {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+          alt="background"
+        />
         <Avatar className="sidebar__avatar" />
         <h2>Jane Doe</h2>
         <h4>jane.doe@gmail.com</h4>
       </div>
       <div className="sidebar__stats">
-        <div className="sidebar__stat"></div>
-        <div className="sidebar__stat"></div>
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,973</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">3,478</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem('test')}
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('software engineering')}
+        {recentItem('design')}
       </div>
     </div>
   );
